9e855c488c23edfe52b1b30b9537ee707e31010d
Ethan <you@example.com>
2021-04-12 17:24:10 -0400

    updated

--- benchmarks/fib_common_expr.lisp
+++ benchmarks/fib_common_expr.lisp
@@ -1,4 +1,4 @@
 (define (fib n)
   (if (< n 3)
       1
-      (+ (fib (- n 1)) (fib (- n 2)))))(print (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) (+ (fib 6) 0))))))))))))
+      (+ (fib (- n 1)) (fib (- n 2)))))(print (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) (+ (fib 7) 0)))))))))))))))))))))
