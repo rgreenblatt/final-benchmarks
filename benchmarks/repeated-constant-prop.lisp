ad6ebe53455993596159841d997563891f747f2f
Charlie Gagnon <47061354+chgagnon@users.noreply.github.com>
2021-04-15 13:13:02 -0400

    Create repeated-constant-prop.lisp

---
+++ benchmarks/repeated-constant-prop.lisp
@@ -0,0 +1,5 @@
+(define (do-a-lot n) (if (= 0 n) 
+                         true 
+                         (do (let ((x (if false true false))
+                                  (if x 1 2))
+                              (do-a-lot (- n 1)))))
