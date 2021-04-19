bf387f5cf88efae332d71441068dac8c9da6e2da
rgreenblatt <greenblattryan@gmail.com>
2021-04-19 15:16:00 -0400

    Make constant folding slightly more obvious

--- benchmarks/const_in_func.lisp
+++ benchmarks/const_in_func.lisp
@@ -1,5 +1,8 @@
-(define (do-math x)
+(define (do-math0 x)
         (+ x (- 19999 (+ 9999 10000))))
+
+(define (do-math x)
+        (+ x (- (do-math0 19999) (+ (do-math0 9999) (do-math0 10000)))))
 
 (define (run-bench passes) 
     (if (< passes 1) (print 0)
